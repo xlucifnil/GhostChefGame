d192f1564fe1fccd9b9d91b41416dfaff4c22ab4
Keegan Buckingham <keegan.buckingham@mymail.champlain.edu>
2024-08-01 20:40:09 -0400

    Got the grid to spawn with random tiles.

--- Assets/Scripts/Matching/MatchingManagement.cs
+++ Assets/Scripts/Matching/MatchingManagement.cs
@@ -6,17 +6,40 @@ public class MatchingManagement : MonoBehaviour
 {
     public GameObject[] tileTypes;
     public int rows, cols;
-    public GameObject[][] board;
+    public float gridSpacing;
+    public GameObject[,] board;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateBoard();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void GenerateBoard()
+    {
+        board = new GameObject[cols, rows];
+        for (int i = 0; i < cols; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                board[i, j] = Instantiate(GenerateTile());
+                board[i, j].transform.position = new Vector2(gameObject.transform.position.x + (i * gridSpacing), gameObject.transform.position.y + (-j * gridSpacing));
+            }
+        }
+    }
+
+    public GameObject GenerateTile()
+    {
+        GameObject tile;
+
+        tile = tileTypes[Random.Range(0,tileTypes.Length)];
+
+        return tile;
     }
 }
