915cfde6b956545c9006c44f91e554c8776f1c70
Keegan Buckingham <keegan.buckingham@mymail.champlain.edu>
2024-08-29 08:13:24 -0400

    Making the player become invulnerable for a short time after taking damage with a visual indicator.

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -7,6 +7,32 @@ public class PlayerStats : MonoBehaviour
     public int maxHealth = 5;
     public int health = 5;
     public int defense = 0;
+    public float invulnDuration = 1;
+    float invulnTime;
+    public bool invulnerable = false;
+    public Color invulnColor = Color.white;
+    Color originalColor = Color.white;
+
+    private void Start()
+    {
+        invulnTime = invulnDuration;
+        originalColor = GetComponent<SpriteRenderer>().color;
+    }
+
+    private void Update()
+    {
+        if (invulnerable)
+        {
+            invulnTime -= Time.deltaTime;
+
+            if (invulnTime <= 0)
+            {
+                invulnTime = invulnDuration;
+                invulnerable = false;
+                GetComponent<SpriteRenderer>().color = originalColor;
+            }
+        }
+    }
 
     public void TakeDamage(int damage)
     {
@@ -20,9 +46,11 @@ public class PlayerStats : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "EnemyAttack")
+        if(collision.gameObject.tag == "EnemyAttack" && !invulnerable)
         {
             TakeDamage(collision.GetComponent<EnemyAttack>().damage);
+            invulnerable = true;
+            GetComponent<SpriteRenderer>().color = invulnColor;
         }
     }
 }
